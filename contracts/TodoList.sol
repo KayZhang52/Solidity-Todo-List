b57069f224b9d810181e72e0d09013882860a9ec
kay <zhangchaofan9627@gmail.com>
2023-09-03 20:04:47 +0800

    set up files

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    /*
+        public keyword makes var accessible from public
+     */
+    uint public taskCount =0; //state variable, a variable that is written in the blockchain
+
+}
